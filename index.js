82cc4421c7b9d88061cd393280e4f76545db0c8e
ShashikantMaury <80745766+shashikantmaurya0007@users.noreply.github.com>
2021-09-23 10:39:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+let input=require('readline-sync');
+var style=require('chalk');
+
+let name=input.question(style.blue("enter your name Gentleman?"));
+console.log(style.green(name)+' welcome here')
+const mumbai=[
+{
+  question:"Which is the most famous festival of mumbai?",
+  option:["Ganpati","holi","durgapuja","eid"],
+  answer:"Ganpati"
+
+},{  question:"Which is the most famous street food of mumbai?",
+  option:["chole","vada-pav","momos","biryani"],
+  answer:"vada-pav"        },{     question:"Which is the most famous sports of mumbai?",
+  option:["cricket","hockey","badmintion","rugby"],
+  answer:"cricket"      },{      question:"What is tag line of mumbai?",
+  option:["city of color","city of lights","people city","city of dream"],
+  answer:"city of dream"          },{      
+     question:"The worst incident happened in  mumbai?",
+  option:["26/11","9/11","2/13","4/12"],
+  answer:"26/11"
+  }
+
+
+
+
+]
+
+
+let score=0;
+
+for (let i=0;i<mumbai.length;i++)
+
+{
+console.log(style.red(mumbai[i].question));
+mumbai[i].option.map((j)=>
+{style.green(console.log(j));})
+var answer=input.question(style.green("enter the answer"));
+if(answer.toLowerCase()==mumbai[i].answer.toLowerCase())
+{
+    score+=1;
+  console.log(style.green("correct answer your current score is  "+score));
+
+}
+else
+{
+   console.log(style.green("wrong answer your current score is"+score));
+}
+
+
+
+
+
+
+}
+
+
+console.log(style.red("thank you for taking part in the quiz"+style.green("you scored"+score+"  "+ (score/5)*100+"%" )))
